6b72c2340cd2f5b5d926879ffe18553c02d7d42c
chae-mg <coalsrl94@gmail.com>
2026-09-10 00:25:14 +0900

    ci: use full chromium for file system e2e

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        channel: process.env.CI ? undefined : 'chrome',
+        channel: process.env.CI ? 'chromium' : 'chrome',
       },
     },
   ],
